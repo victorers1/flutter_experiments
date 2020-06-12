3ab828e70c0dad5a6a048cee501f9779cb00c567
Victor Emanuel <victorers2@gmail.com>
2020-06-12 13:10:05 -0300

    feat(pubspec.yaml): adds image_picker ^0.6.7+1

--- lib/main.dart
+++ lib/main.dart
@@ -17,53 +17,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/home': (context) => HomePage(),
       },
-      home: MyHomePage(title: 'Tela inicial'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Você pressionou o botão essa quantidade de vezes:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Dica',
-        child: Icon(Icons.add),
-      ),
+      initialRoute: '/home',
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,7 +5,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home page'),
+        title: Text('Image Picker demo'),
       ),
       body: Container(
         child: Center(
