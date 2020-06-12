58ac9ec821166e9fecddd20d2265ff0d74130e50
Victor Emanuel <victorers2@gmail.com>
2020-06-12 16:35:47 -0300

    feat(main.dart): adds working example of image_picker package
    user can pick from camera or gallery, image is seen right below

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,17 +1,77 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  File image;
+  final _picker = ImagePicker();
+
+  Future _getImageFromGallery() async {
+    final pickedFile = await _picker.getImage(source: ImageSource.gallery);
+    print('path: ${pickedFile.path}');
+    setState(() {
+      image = File(pickedFile.path);
+    });
+  }
+
+  Future _getImageFromCamera() async {
+    final pickedFile = await _picker.getImage(source: ImageSource.camera);
+    print('path: ${pickedFile.path}');
+    setState(() {
+      image = File(pickedFile.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Image Picker demo'),
       ),
-      body: Container(
-        child: Center(
-          child: Text('Home page'),
-        ),
+      body: ListView(
+        padding: EdgeInsets.only(top: 20, left: 16, right: 16),
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text('Pick from:'),
+              // SizedBox(width: 20),
+              RaisedButton(
+                  onPressed: _getImageFromCamera, child: Text('Camera')),
+              // SizedBox(width: 20),
+              RaisedButton(
+                  onPressed: _getImageFromGallery, child: Text('Gallery')),
+            ],
+          ),
+          SizedBox(height: 50),
+          image != null ? Image.file(image) : Text('No image selected'),
+        ],
       ),
     );
   }
 }
+
+/**
+ * 
+ * SingleChildScrollView(
+        child: Center(
+          child: Expanded(
+                    child: Column(
+              children: <Widget>[
+                RaisedButton(onPressed: _getImageFromCamera, child: Text('Camera')),
+                RaisedButton(
+                    onPressed: _getImageFromGallery, child: Text('Gallery')),
+                SizedBox(height: 50),
+                image != null ? Image.file(image) : Text('No image selected'),
+              ],
+            ),
+          ),
+        ),
+      ),
+ */
