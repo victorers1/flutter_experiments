d187febddb512445855fc180924872eb1f10e926
Victor Emanuel <victorers2@gmail.com>
2020-06-14 15:04:26 -0300

    refactor(main.dart): adds cleaner initial page

--- lib/main.dart
+++ lib/main.dart
@@ -18,52 +18,32 @@ class MyApp extends StatelessWidget {
       routes: {
         '/home': (context) => HomePage(),
       },
-      home: MyHomePage(title: 'Tela inicial'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Initial Screen'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Você pressionou o botão essa quantidade de vezes:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text('Place your widgets here'),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Dica',
-        child: Icon(Icons.add),
       ),
     );
   }
