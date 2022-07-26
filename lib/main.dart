d7104849dc054118d00e4c7837431ebd45f6b244
Victor Emanuel <victor.emanuel@kobe.io>
2022-07-25 21:21:57 -0300

    dds credit card scanner

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,37 @@
-import 'package:experiment/pages/home.dart';
+import 'package:credit_card_scanner/credit_card_scanner.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as dev;
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/home': (context) => const HomePage(),
-      },
-      home: const MyHomePage(),
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _cardScanOptions = const CardScanOptions(
+    enableDebugLogs: true,
+    scanCardHolderName: true,
+    scanExpiryDate: true,
+    cardScannerTimeOut: 30000,
+  );
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  String cardDetails = 'Nenhum cartão';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,18 +39,25 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Você pressionou o botão essa quantidade de vezes:',
+            ElevatedButton(
+              onPressed: () async {
+                var cardDetails = await CardScanner.scanCard(
+                  scanOptions: _cardScanOptions,
+                );
+                setState(() {
+                  this.cardDetails = cardDetails.toString();
+                });
+                dev.log(this.cardDetails);
+              },
+              child: const Text('Analisar Cartão'),
             ),
+            const SizedBox(height: 40),
             Text(
-              '$_counter',
+              cardDetails,
+              style: Theme.of(context).textTheme.bodyMedium,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add),
       ),
     );
   }
